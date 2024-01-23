bc7511c5d23bb018171deccef606bd1d77917586
KonstantinS <kostiantyn.soroka@jamesandjames.com>
2024-01-23 22:55:47 +0200

    code from lesson #2

--- file1.php
+++ file1.php
@@ -1,29 +1,27 @@
 <?php
-$name = 'John 2'; //$_POST['name']
-$age = 12;
-$street = 'Main Avenue 20';
 
-$delimeter = '|-|';
-$stringWithData = $name . $delimeter . $age . $delimeter . $street;
-//echo $stringWithData .PHP_EOL;
-//'John|-|9|-|Main Avenue 21';
-//$dataFromString = explode($delimeter, $stringWithData);
-//var_dump($dataFromString);
-//$test = implode(', ', $dataFromString);
+$first = 89798789;
+$second = 2131231;
 
-//$fileToFill = fopen("data.txt", "a+") or die("Couldn't read the file");
-//fwrite($fileToFill, $stringWithData . PHP_EOL);
-//fclose($fileToFill);
-
-$fileToRead = fopen("data.txt", "r") or die("Couldn't read the file");
-
-while(!feof($fileToRead)) {
-    $dataFromRow = fgets($fileToRead);
-    $dataFromString = explode($delimeter, $dataFromRow);
-
-    if (isset($dataFromString[1]) && $dataFromString[1] == 12) {
-        echo "Hello Mr." . $dataFromString[0] . ' from ' . $dataFromString[2] . PHP_EOL;
-    }
+function difference()
+{
+    return $GLOBALS['first'] - $GLOBALS['second'];
 }
 
-fclose($fileToRead);
+function secondDiff()
+{
+    $GLOBALS;
+    $_SERVER;
+    $_GET;
+    $_POST;
+    $_FILES;
+    $_COOKIE;
+    $_SESSION;
+    $_REQUEST;
+    $_ENV;
+}
+
+
+echo difference();
+
+echo PHP_EOL;
