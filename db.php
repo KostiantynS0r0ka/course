7c9775a8d8a213d86274b2b8433217ae25d62e2f
Kostiantyn Soroka <kostiantyn.soroka@gimo.co.uk>
2024-02-15 18:30:00 +0200

    PSEUDO code from lection

--- db.php
+++ db.php
@@ -50,11 +50,36 @@ function addNewMessage(PDO $pdo, string $name, string $message)
 
 function deleteMessage($pdo, $id)
 {
+    $id = 1234;//DELETE FROM messages WHERE id=1234
+    $id = "''; TRUNCATE TABLE messages;"; // DELETE FROM messages WHERE id=''; TRUNCATE TABLE messages;
+    $sqlBad = "DELETE FROM messages WHERE id=" . $id;
+
     $sql = "DELETE FROM messages WHERE id=:id";
 
     $queryRunner = $pdo->prepare($sql);
+    $pdo->query($sqlBad);
 
     if (!$queryRunner->execute(['id' => $id])) {
         echo 'Something went wrong';
     }
+}
+
+function checkAuth(PDO $PDO, string $login, string $password)
+{
+    $sqlBad = "SELECT * FROM users WHERE login=$login AND password=$password";
+    $login = 'admin'; $password = '123123123'; // SELECT * FROM users WHERE login='admin' AND password='123123123'
+    $login = "'' OR '1'='1'"; $password = '123123123'; // SELECT * FROM users WHERE login='' OR '1'='1' AND password='123123123'
+
+
+    $sql = "SELECT * FROM users WHERE login=:login AND password=:password";
+    $queryRunner = $PDO->prepare($sql);
+    $users = $queryRunner->execute(['login' => $login, 'password' => $password]);
+    if ($users && $users[0]) {
+        $_SESSION['is_auth'] = true;
+        //... $users[0]
+    }
+    //execute
+
+    //check user/data. if data exists === auth success
+
 }
